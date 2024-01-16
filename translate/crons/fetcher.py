d5a7780447940d8e787ac2cb386fc9eea2ca3107
Amin Saedi <amin.di.sam@gmail.com>
2024-01-16 11:23:38 -0500

    Fixed is_special_char

--- translate/crons/fetcher.py
+++ translate/crons/fetcher.py
@@ -6,32 +6,44 @@ from django.db.models import Count
 from django.db.models.functions import Lower
 import subprocess
 import json
-from ..models import Language, ApplicationString, ApplicationStringsTranslation, DuplicateString
+from ..models import (
+    Language,
+    ApplicationString,
+    ApplicationStringsTranslation,
+    DuplicateString,
+)
 from ..apps import TranslateConfig
 
 BASE_DIR = str(settings.BASE_DIR)
 APP_NAME = TranslateConfig.name
 
+
 def update_translation_strings():
     try:
-        shutil.rmtree(BASE_DIR + f'/{APP_NAME}/temp/frontendFetch')
+        shutil.rmtree(BASE_DIR + f"/{APP_NAME}/temp/frontendFetch")
     except FileNotFoundError:
         pass
-    Repo.clone_from(settings.GIT_URL, BASE_DIR + f'/{APP_NAME}/temp/frontendFetch')
+    Repo.clone_from(settings.GIT_URL, BASE_DIR + f"/{APP_NAME}/temp/frontendFetch")
 
-    result = subprocess.run(['./extract.sh'], cwd=f'{BASE_DIR}/{APP_NAME}/temp/frontendFetch', stdout=subprocess.PIPE)
+    result = subprocess.run(
+        ["./extract.sh"],
+        cwd=f"{BASE_DIR}/{APP_NAME}/temp/frontendFetch",
+        stdout=subprocess.PIPE,
+    )
     """
         Check if the command was successful
     """
     if result.returncode != 0:
-        raise Exception('Error while extracting translation strings')
+        raise Exception("Error while extracting translation strings")
     """
         Read the extracted translation strings
     """
-    with open(f'{BASE_DIR}/{APP_NAME}/temp/frontendFetch/extractLang/en.json', 'r') as f:
+    with open(
+        f"{BASE_DIR}/{APP_NAME}/temp/frontendFetch/extractLang/en.json", "r"
+    ) as f:
         data = json.load(f)
     try:
-        shutil.rmtree(BASE_DIR + f'/{APP_NAME}/temp/frontendFetch')
+        shutil.rmtree(BASE_DIR + f"/{APP_NAME}/temp/frontendFetch")
     except FileNotFoundError:
         pass
     """
@@ -45,49 +57,49 @@ def update_translation_strings():
 
     """
 
-
     for formatjs_id, translation_string in data.items():
         try:
             ApplicationString.objects.get(formatjs_id=formatjs_id)
         except ApplicationString.DoesNotExist:
             translation_string = ApplicationString(
                 formatjs_id=formatjs_id,
-                default_message=translation_string.get('defaultMessage') or 'ERROR',
-                description=translation_string.get('description'),
+                default_message=translation_string.get("defaultMessage") or "ERROR",
+                description=translation_string.get("description"),
+                is_special_char=translation_string.get("defaultMessage").count("{") > 0,
             )
             translation_string.save()
 
             ApplicationStringsTranslation(
                 translation_string=translation_string,
-                language=Language.objects.get(code='en-US'),
+                language=Language.objects.get(code="en-US"),
                 string=translation_string.default_message,
-                is_approved=False
+                is_approved=False,
             ).save()
 
-            for other_language in Language.objects.exclude(code='en-US'):
+            for other_language in Language.objects.exclude(code="en-US"):
                 ApplicationStringsTranslation(
                     translation_string=translation_string,
                     language=other_language,
-                    string='',
-                    is_approved=False
+                    string="",
+                    is_approved=False,
                 ).save()
 
     for application_string in ApplicationString.objects.all():
         if application_string.formatjs_id not in data.keys():
-            print(application_string.formatjs_id + ' deleted')
+            print(application_string.formatjs_id + " deleted")
             application_string.delete()
 
     DuplicateString.objects.all().delete()
 
-    duplicates = ApplicationString.objects.annotate(message_lower=Lower("default_message")).values("message_lower").annotate(count=Count("message_lower"))
+    duplicates = (
+        ApplicationString.objects.annotate(message_lower=Lower("default_message"))
+        .values("message_lower")
+        .annotate(count=Count("message_lower"))
+    )
 
     for duplicate in duplicates:
-        if duplicate['count'] > 1:
+        if duplicate["count"] > 1:
             duplicate_string = DuplicateString(
-                default_message=duplicate['message_lower'],
-                count=duplicate['count']
+                default_message=duplicate["message_lower"], count=duplicate["count"]
             )
             duplicate_string.save()
-
-    
-    
--- translate/crons/translator.py
+++ translate/crons/translator.py
@@ -1,7 +1,12 @@
 from deep_translator import GoogleTranslator
 from datetime import datetime
 from django.db.models import Q
-from ..models import ApplicationStringsTranslation, AutoTranslationLog, GraphqlEnumValueTranslation, CustomKeyTranslation
+from ..models import (
+    ApplicationStringsTranslation,
+    AutoTranslationLog,
+    GraphqlEnumValueTranslation,
+    CustomKeyTranslation,
+)
 
 
 def get_target_lang(full_lang_code):
@@ -11,31 +16,36 @@ def get_target_lang(full_lang_code):
     :return: source language
     """
 
-    if full_lang_code == 'he-IL':
-        return 'hebrew'
+    if full_lang_code == "he-IL":
+        return "hebrew"
 
-    return full_lang_code.split('-')[0]
+    return full_lang_code.split("-")[0]
 
 
 def translate_all_strings():
-    log = AutoTranslationLog(
-        start_time=datetime.now(),
-        total_translated=0
-    )
+    log = AutoTranslationLog(start_time=datetime.now(), total_translated=0)
     log.save()
 
     total = 0
-    for string in ApplicationStringsTranslation.objects.filter(translation_string__is_special_char=False, is_approved=False, string='').exclude(language__code='en-US'):
-        string.string = GoogleTranslator(source='en', target=get_target_lang(
-            string.language.code)).translate(string.translation_string.default_message)
+    for string in ApplicationStringsTranslation.objects.filter(
+        translation_string__is_special_char=False, is_approved=False, string=""
+    ).exclude(language__code="en-US"):
+        string.string = GoogleTranslator(
+            source="en", target=get_target_lang(string.language.code)
+        ).translate(string.translation_string.default_message)
+        if not string.string:
+            string.string = string.translation_string.default_message
         string.save()
         total += 1
         log.total_translated = total
         log.save()
 
-    for string in ApplicationStringsTranslation.objects.filter(translation_string__is_special_char=True, is_approved=False).exclude(language__code='en-US'):
+    for string in ApplicationStringsTranslation.objects.filter(
+        translation_string__is_special_char=True, is_approved=False
+    ).exclude(language__code="en-US"):
         string.string = ApplicationStringsTranslation.objects.get(
-            translation_string=string.translation_string, language__code='en-US').string
+            translation_string=string.translation_string, language__code="en-US"
+        ).string
         string.save()
 
     log.end_time = datetime.now()
@@ -47,26 +57,34 @@ def translate_all_enums():
     log = AutoTranslationLog(
         start_time=datetime.now(),
         total_translated=0,
-        target_type=AutoTranslationLog.TARGET_TYPE_CHOICES[1][0]
+        target_type=AutoTranslationLog.TARGET_TYPE_CHOICES[1][0],
     )
     log.save()
 
     total = 0
-    for enum in GraphqlEnumValueTranslation.objects.filter(is_approved=False, string='').exclude(language__code='en-US'):
+    for enum in GraphqlEnumValueTranslation.objects.filter(
+        is_approved=False, string=""
+    ).exclude(language__code="en-US"):
         english_translation = GraphqlEnumValueTranslation.objects.get(
-            enum_value=enum.enum_value, language__code='en-US')
-        enum.string = GoogleTranslator(source='en', target=get_target_lang(
-            enum.language.code)).translate(english_translation.string)
+            enum_value=enum.enum_value, language__code="en-US"
+        )
+        enum.string = GoogleTranslator(
+            source="en", target=get_target_lang(enum.language.code)
+        ).translate(english_translation.string)
         enum.save()
         total += 1
         log.total_translated = total
         log.save()
 
-    for custom_key in CustomKeyTranslation.objects.filter(is_approved=False, string='').exclude(language__code='en-US'):
+    for custom_key in CustomKeyTranslation.objects.filter(
+        is_approved=False, string=""
+    ).exclude(language__code="en-US"):
         english_translation = CustomKeyTranslation.objects.get(
-            custom_key=custom_key.custom_key, language__code='en-US')
-        custom_key.string = GoogleTranslator(source='en', target=get_target_lang(
-            custom_key.language.code)).translate(english_translation.string)
+            custom_key=custom_key.custom_key, language__code="en-US"
+        )
+        custom_key.string = GoogleTranslator(
+            source="en", target=get_target_lang(custom_key.language.code)
+        ).translate(english_translation.string)
         custom_key.save()
         total += 1
         log.total_translated = total
